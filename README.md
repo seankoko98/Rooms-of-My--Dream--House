dbe3739e72bbb2411d0f494b792c412710ab502d
seankoko98 <78056052+seankoko98@users.noreply.github.com>
2021-02-23 14:16:26 -0800

    added files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Rooms of My "Dream" House
- designing my dream house in xd and p5.js
+## Mood States
+#### by Sean Ko
+
+
+### Overview
+This is a simple state machine that goes between 5 mood states 
+
+
+### Modifying the Template
+
+(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
+
+(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
+
+(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
+
+Don't change anything else! 
+
+### How it works
---
+++ sketch.js
@@ -0,0 +1,187 @@
+/***********************************************************************************
+	SimpleStateMachine - TEMPLATE
+	by Scott Kildall
+
+	Template:
+
+	(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
+	(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
+	(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
+
+	Also start your localhost before running this, otherwise no PNGs will display
+
+------------------------------------------------------------------------------------
+	The way it works — you don't need to know this for the template use
+	* array of images gets loaded at startup
+	* drawFunction is a VARIABLE that points to a function varible name
+	* drawOne(), drawTwo(), etc. are set to be functions.
+	* the the keys 1-5 will change the drawFunction variable
+
+------------------------------------------------------------------------------------
+	Notes:
+	- a more advanced state machine with use array-indexing for each of
+		images the draw functions, but this is just for illustrative purposes
+
+	- even more advanced will be to put the draw functions into an array, would
+		be helpful for randomizing, go to the next function, etc
+
+	- next step after that would be to put interfaces into an array that maps to
+		the functions
+
+
+***********************************************************************************/
+
+// Array of images
+var images = [];
+
+var instructions = []; 
+
+// variables used for instructions 
+var midX; 
+var startY;
+var lineHeight = 48;
+
+// variable that is a function 
+var drawFunction;
+
+// offset from bottom of screen
+var gTextOffset = 20;
+
+// load all images into an array
+function preload() {
+  images[0] = loadImage('assets/one.png');
+  images[1] = loadImage('assets/two.png');
+  images[2] = loadImage('assets/three.png');
+  images[3] = loadImage('assets/four.png');
+  images[4] = loadImage('assets/five.png');
+  images[5] = loadImage('assets/splash.png');
+}
+
+
+function loadInstructionsArray() {
+  instructions[0] = "Press 1 for relaxing"; 
+  instructions[1] = "Press 2 for enlightening"; 
+  instructions[2] = "Press 3 for calm"; 
+  instructions[3] = "Press 4 for cheerful";
+  instructions[4] = "Press 5 for comforting"
+  instructions[5] = "Press s for the mood states splash screen"; 
+  instructions[6] = "When on the splash screen click to get the instructions screen or press i"; 
+}
+
+// Center drawing, drawFunction will be one for default
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+// loading all arrays 
+  loadInstructionsArray(); 
+
+  midX = width/2;
+  startY = 60;
+
+  // Center our drawing objects
+  imageMode(CENTER);
+  textAlign(CENTER);
+  textSize(24);
+
+  // set to one for startup
+  drawFunction = drawSplash;
+}
+
+// Very simple, sets the background color and calls your state machine function
+function draw() {
+  background(192);
+
+  // will call your state machine function
+  drawFunction();
+}
+
+//========= TEMPLATE: modify these functions, INSIDE the function blocks only =========
+
+//-- drawOne() will draw the image at index 0 from the array
+drawOne = function() {
+   image(images[0],width/2, height/2);
+
+   fill(0,0,0);
+   text("relaxing", width/2, height - gTextOffset);
+}
+
+//-- drawTwo() will draw the image at index 1 from the array
+drawTwo = function() {
+   image(images[1],width/2, height/2);
+
+   fill(240,120,0);
+   text("enlightening", width/2, height - gTextOffset);
+}
+
+//-- drawOne() will draw the image at index 2 from the array
+drawThree = function() {
+   image(images[2],width/2, height/2);
+
+   fill(40,230,120);
+   text("calm", width/2, height - gTextOffset);
+}
+
+//-- drawOne() will draw the image at index 3 from the array
+drawFour = function() {
+   image(images[3],width/2, height/2);
+
+   fill(255,255,178);
+   text("cheerful", width/2, height - gTextOffset);
+}
+
+//-- drawOne() will draw the image at index 4 from the array
+drawFive = function() {
+   image(images[4],width/2, height/2);
+
+   fill(230,50,50);
+   text("comforting", width/2, height - gTextOffset);
+}
+
+//-- drawSplash() will draw the image at index 4 from the array
+drawSplash = function() {
+   image(images[5],width/2, height/2);
+}
+
+// this will display instructions screen
+drawInstructions = function() {
+  for( let i = 0; i < instructions.length; i++ ) {
+    fill(255); 
+    text( instructions[i], midX, startY + (i * lineHeight) )
+  }
+}
+
+
+// /
+
+// //========= TEMPLATE: add or change interface functions, as you like =========
+
+// Change the drawFunction variable based on your interaction
+function keyTyped() {
+  if( key === '1' ) {
+  	drawFunction = drawOne;
+  }
+  else if( key === '2' ) {
+  	drawFunction = drawTwo;
+  }
+  else if( key === '3' ) {
+  	drawFunction = drawThree;
+  }
+  else if( key === '4' ) {
+  	drawFunction = drawFour;
+  }
+  else if( key === '5' ) {
+  	drawFunction = drawFive;
+  }
+  else if( key == 's') {
+    drawFunction = drawSplash; 
+  }
+  else if( key == 'i'){
+  	drawFunction = drawInstructions;
+  }
+}
+
+function mousePressed() {
+  // only change state if we are in splash screen
+  if( drawFunction === drawSplash ) {
+    drawFunction = drawInstructions;
+  }
+}
